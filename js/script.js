06c9e2c30a854133cf21dc63d5004380dccc6eea
abrsh <abrshkorra@gmail.com>
2022-10-18 13:05:26 +0300

    overlay for the hamburger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-!
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+
+btn.addEventListener('click', navToogle);
+
+function navToogle(){
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show');
+    document.body.classList.toggle('stop-scrolling');
+}
